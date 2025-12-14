6e2d450acbe2788e696635c293c9b5f6a2f75c05
caninkostya-wq <canin.kostya@gmail.com>
2025-12-14 20:35:06 +0300

    new module test_module.py in tests

--- tests/test_module.py
+++ tests/test_module.py
@@ -1,1 +1,25 @@
+# test_widget.py — расширенный тест
+
 import widget.py
+
+
+# Список тестовых данных
+test_cases = [
+    "Maestro 1596837868705199",
+    "Счет 64686473678894779589",
+    "MasterCard 7158300734726758",
+    "Счет 35383033474447895560",
+    "Visa Classic 6831982476737658",
+    "Visa Platinum 8990922113665229",
+    "Visa Gold 5999414228426353",
+    "Счет 73654108430135874305"
+]
+
+print("🔍 Тестирование mask_account_card на нескольких строках:\n")
+
+for case in test_cases:
+    try:
+        result = widget.mask_account_card(case)
+        print(f"{case} → {result}")
+    except Exception as e:
+        print(f"{case} → ОШИБКА: {e}")
